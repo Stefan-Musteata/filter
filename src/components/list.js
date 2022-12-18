d1e4ff7ee45cf39da1fd4f96e598a2cfd5831ed8
stefanel1990 <musteata.stefan90@gmail.com>
2022-12-18 17:54:22 +0200

    created link

--- src/components/list.js
+++ src/components/list.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const list = ({ teams }) => {
+
+  //Name to slug
+  const slugify = (str) => {
+    return str
+      .toLowerCase()
+      .trim()
+      .replace(/[^\w\s-]/g, '')
+      .replace(/[\s_-]+/g, '-')
+      .replace(/^-+|-+$/g, '')
+  }
+
   return (
     <div className="table">
       <ul>
         {teams.map((team, i) => (
-          <li key={i}>{i + 1}. {team}</li>
+          <li key={i} >
+            {i + 1}. <Link to={`/page/${slugify(team)}/`}  >{team}</Link>
+          </li>
         ))}
       </ul>
     </div>
